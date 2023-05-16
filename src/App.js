7c3c0d89bb15dda042f4cba3645bf6a8226d2afe
Sagar Jadhav <77880641+sagarmj94@users.noreply.github.com>
2023-05-16 15:38:15 +0530

    Add files via upload
    changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Container from "react-bootstrap/esm/Container";
+import "./App.css";
+import Navigation from "./component/Navigation";
+import Caritage from "./component/Caritage";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Caritage />
+    </>
+  );
+}
+
+export default App;
